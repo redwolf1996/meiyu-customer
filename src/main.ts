bfcae96b9bef5fa8ef29d05ddc6596ff132702a3
syx9527 <770460616@qq.com>
2025-04-28 08:13:56 +0800

    go

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,6 @@ import { createSSRApp } from 'vue'
 import App from './App.vue'
 import pinia from './stores'
 import 'uno.css'
-import VConsole from 'vconsole'
-
-const vConsole = new VConsole() as any
-vConsole.init()
 
 export function createApp() {
   const app = createSSRApp(App)
